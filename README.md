c42a12a25ef83610083c3ad95492ade038b208e7
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2023-11-23 14:52:28 +0900

    frontend buttons added

---
+++ README.md
@@ -0,0 +1,2 @@
+![img](screen1.png)
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,8 +20,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+
+def my_view_sync(request):
+    # 요청 처리 로직
+    return JsonResponse({'message': '동기 처리 응답 메시지 : 항상 완료'})
+
+def my_view_async(request):
+    # 요청 처리 로직
+    return JsonResponse({'message': '비동기 처리 응답 메시지 : 상태 시작했음'})
+
+
+def my_view_async_status(request):
+    # 요청 처리 로직
+    return JsonResponse({'message': '비동기 처리 응답 메시지 : 상태 완료했음'})
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", cache_page(60 * 15)(TemplateView.as_view(template_name="home.html")), name="home"),
+    path('path-to-your-backend-endpoint-sync', my_view_sync, name='my_view_sync'),
+    path('path-to-your-backend-endpoint-async', my_view_async, name='my_view_async'),
+    path('path-to-your-backend-endpoint-async-status', my_view_async_status, name='my_view_async_status'),
 ]
